2752700214d11ebc893d8ac9dbd32ad2d69b8bb6
Biswa Mitra Barua <biswamitra960@gmail.com>
2024-09-23 19:55:06 +0600

    added all cards fuctionalities with modals and validations

--- js/index.js
+++ js/index.js
@@ -40,3 +40,76 @@ donateNoakhaliBtn.addEventListener('click', function () {
 
 })
 
+// ----Feni card---
+const donateFeniBtn = document.getElementById('donate-feni-btn');
+donateFeniBtn.addEventListener('click', function () {
+
+    const amountDonationFeni = getInputValueById('input-feni');
+    const myAmount = document.getElementById('my-amount');
+    const newMyAmount = myAmount.innerText;
+
+    if (newMyAmount <= 0 || amountDonationFeni > newMyAmount) {
+        alert('You do not have sufficient balance');
+        const myModal = document.getElementById('my_modal_2').classList.add('hidden');
+        window.location.reload();
+        return;
+    }
+    else {
+        if (amountDonationFeni <= 0 || isNaN(amountDonationFeni)) {
+            alert('Please provide the positive amount');
+            const myModal = document.getElementById('my_modal_2').classList.add('hidden');
+            window.location.reload();
+            return;
+        }
+        else {
+            const amountFeni = document.getElementById('amount-feni');
+            const newAmountFeni = Number((amountFeni).innerText);
+
+            const totalAmountFeni = newAmountFeni + amountDonationFeni;
+            amountFeni.innerText = totalAmountFeni;
+
+            const finalMyAmount = newMyAmount - amountDonationFeni;
+            myAmount.innerText = finalMyAmount;
+        }
+    }
+
+})
+
+
+// ----Quota card---
+const donateQuotaBtn = document.getElementById('donate-quota-btn');
+donateQuotaBtn.addEventListener('click', function () {
+
+    const amountDonationQuota = getInputValueById('input-quota');
+    const myAmount = document.getElementById('my-amount');
+    const newMyAmount = myAmount.innerText;
+
+    if (newMyAmount <= 0 || amountDonationQuota > newMyAmount) {
+        alert('You do not have sufficient balance');
+        const myModal = document.getElementById('my_modal_3').classList.add('hidden');
+        window.location.reload();
+        return;
+    }
+    else {
+        if (amountDonationQuota <= 0 || isNaN(amountDonationQuota)) {
+            alert('Please provide the positive amount');
+            const myModal = document.getElementById('my_modal_3').classList.add('hidden');
+            window.location.reload();
+            return;
+        }
+        else {
+            const amountQuota = document.getElementById('amount-quota');
+            const newAmountQuota = Number((amountQuota).innerText);
+
+            const totalAmountQuota = newAmountQuota + amountDonationQuota;
+            amountQuota.innerText = totalAmountQuota;
+
+            const finalMyAmount = newMyAmount - amountDonationQuota;
+            myAmount.innerText = finalMyAmount;
+
+        }
+    }
+
+})
+
+
